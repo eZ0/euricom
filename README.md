cc026afe0cc0b4156d1df80d2fa33aa360cea83f
Ksenia Karelskaya <kusksu@gmail.com>
2015-06-29 23:15:15 +0200

    on click Angularjs event added

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Euricom - AngularJS tests
 
 Some changes:
-####1. Fetching data from JSON with [AngularJS]( http://angularjs.org )
-####2. Web page is responsive (using Bootstrap)
-####3. ...
+1. Fetching data from JSON with [AngularJS]( http://angularjs.org )
+2. Web page is responsive ([Bootstrap]( http://getbootstrap.com ))
+3. ...
 
 
 [Online version]( http://ksenia.be/euricom )
--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 // Initializing our app and passing in dependencies
-angular.module("teamApp", []).filter('phoneNumber', function() {
-		return function(value) {
-			var s = value.toString();
-			var newNumber = '+' + s.substring(2, 4) 
-					+ ' ' + s.substring(4,7) 
-					+ ' ' + s.substring(7,9) 
-					+ ' ' + s.substring(9,11)
-					+ ' ' + s.substring(11);
-			return newNumber;
-		};
-	})
-	.controller("teamController", function teamController($scope, $http){
-		$http.get('data/data.json').
-			success(function(data, status, headers, config) {
-				$scope.members = data;
-			}).
-			error(function(data, status, headers, config) {
-			// log error
-			});
-	});
+var teamApp = angular.module("teamApp", []);
+
+//custom phone formatting
+teamApp.filter('phoneNumber', function() {
+	return function(value) {
+		var s = value.toString();
+		var newNumber = '+' + s.substring(2, 4) 
+				+ ' ' + s.substring(4,7) 
+				+ ' ' + s.substring(7,9) 
+				+ ' ' + s.substring(9,11)
+				+ ' ' + s.substring(11);
+		return newNumber;
+	};
+});
+
+// retrieving data from Json file
+teamApp.controller("teamController", function getTeam($scope, $http){
+	$http.get('data/data.json').
+		success(function(data, status, headers, config) {
+			$scope.members = data;
+		}).
+		error(function(data, status, headers, config) {
+		// log error
+		});
+	$scope.sendSMS = function(){
+		console.log("clicked!");
+	}
+});
