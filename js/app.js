3719b949c94f9cd1d4da18cc7594bd12b24ea969
Ksenia Karelskaya <kusksu@gmail.com>
2015-06-29 22:30:31 +0200

    Custom filter for number formatting added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 // Initializing our app and passing in dependencies
-angular.module("teamApp", [])
+angular.module("teamApp", []).filter('phoneNumber', function() {
+		return function(value) {
+			var s = value.toString();
+			var newNumber = '+' + s.substring(2, 4) 
+					+ ' ' + s.substring(4,7) 
+					+ ' ' + s.substring(7,9) 
+					+ ' ' + s.substring(9,11)
+					+ ' ' + s.substring(11);
+			return newNumber;
+		};
+	})
 	.controller("teamController", function teamController($scope, $http){
 		$http.get('data/data.json').
 			success(function(data, status, headers, config) {
